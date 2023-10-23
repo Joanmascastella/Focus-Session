a2ebe1e5925e3e9a59df1776bb9ba525c9004b8b
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2023-10-23 23:22:04 +0200

    Creating design

--- src/main/java/com/mas/joan/focussession/Controller/FocusSessionController.java
+++ src/main/java/com/mas/joan/focussession/Controller/FocusSessionController.java
@@ -5,13 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ProgressIndicator;
 
 public class FocusSessionController {
-    @FXML
-    private ProgressIndicator circularProgress;
 
-    public void initialize() {
-        // You can set progress value programmatically here
-        // For example: set progress to 50%
-        circularProgress.setProgress(0.5);
-    }
+
+
 }
 
