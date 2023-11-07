33f83c3f6061b510affbb5f82a35c8b23a0344aa
joanasdasd <33759606+Joanmascastella@users.noreply.github.com>
2023-11-07 13:18:16 +0100

    fixing bugs

--- src/main/java/com/mas/joan/focussession/Controller/MainViewController.java
+++ src/main/java/com/mas/joan/focussession/Controller/MainViewController.java
@@ -1,41 +1,75 @@
 package com.mas.joan.focussession.Controller;
 
 import com.mas.joan.focussession.Database.Database;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
 
-public class MainViewController implements Initializable {
+public class MainViewController {
 
-    @FXML private Button focusButton;
-    @FXML private Button tasksButton;
-    @FXML private Button overviewButton;
-    @FXML private VBox newViewContainer;
-    @FXML private Label message;
+    @FXML
+    private Button focusButton;
+    @FXML
+    private Button tasksButton;
+    @FXML
+    private Button overviewButton;
+    @FXML
+    private VBox newViewContainer;
+    @FXML
+    private Label message;
     private Database database;
+
     public void setDatabase(Database database) {
         this.database = database;
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
+    public void initialize() {
+        try {
+            loadDefaultView();
+        } catch (Exception e) {
+            message.setText("Error initializing the default view.");
+        }
     }
 
-    public void onFocusButtonClick(ActionEvent actionEvent) {
+    private void loadDefaultView() {
+        onFocusButtonClick();
     }
 
-    public void onTasksButtonClick(ActionEvent actionEvent) {
+    private void loadView(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            VBox navigationView = loader.load();
+            Object controller = loader.getController();
+            if (controller instanceof FocusController focusController) {
+                focusController.setDatabase(database);
+            } else if (controller instanceof OverviewController overviewController) {
+                overviewController.setDatabase(database);
+            } else if (controller instanceof TaskController taskController) {
+                taskController.setDatabase(database);
+            }
+            newViewContainer.getChildren().setAll(navigationView);
+        } catch (IOException e) {
+            message.setText("Unable to find view");
+        } catch (Exception e) {
+            message.setText("Error loading the view.");
+        }
     }
 
-    public void onOverviewButtonClick(ActionEvent actionEvent) {
+    public void onFocusButtonClick() {
+        loadView("/com/mas/joan/focussession/FocusView.fxml");
+    }
+
+    public void onTasksButtonClick() {
+        loadView("/com/mas/joan/focussession/TaskView.fxml");
+    }
+
+    public void onOverviewButtonClick() {
+        loadView("/com/mas/joan/focussession/OverviewView.fxml");
     }
 
 
--- src/main/java/com/mas/joan/focussession/Controller/TaskController.java
+++ src/main/java/com/mas/joan/focussession/Controller/TaskController.java
@@ -1,4 +1,10 @@
 package com.mas.joan.focussession.Controller;
 
+import com.mas.joan.focussession.Database.Database;
+
 public class TaskController {
+    private Database database;
+    public void setDatabase(Database database) {
+        this.database = database;
+    }
 }
